401bc85fe4718b88c5b9b1aeebf911e239d46d04
Alexandru Stelea <alexandrustelea@yahoo.com>
2022-04-19 15:05:17 +0300

    Am creat un RestApi basic si adaugat putin html/css

--- DataIngestion/dataIngestion.py
+++ DataIngestion/dataIngestion.py
@@ -5,44 +5,46 @@ import psycopg2
 import schedule
 import json
 
-#VARIABILA DE TIMP PENTRU REFRESHul DB
-refresh_time = 8 #days
+# VARIABILA DE TIMP PENTRU REFRESHul DB
+refresh_time = 8  # days
 
-#DATABASE INFO
+# DATABASE INFO
 dbInf = open('DataIngestion/databaseInfo.json')
 db_info = json.load(dbInf)
 hostname = db_info['hostname']
 database = db_info['databaseName']
 username = db_info['username']
-pwd      = db_info['password']
-port_id  = db_info['portID']
+pwd = db_info['password']
+port_id = db_info['portID']
 dbInf.close()
 
-#AUTENTIFICARE COGNITE
+# AUTENTIFICARE COGNITE
 cognInf = open('DataIngestion/cogniteClientInfo.json')
 client_info = json.load(cognInf)
 api_key = client_info['APIKey']
 client_name = client_info['clientName']
 project_name = client_info['projectName']
 cognInf.close()
-c = CogniteClient(api_key=api_key, client_name=client_name,project=project_name)
+c = CogniteClient(api_key=api_key, client_name=client_name, project=project_name)
 
 
-#FUNCTIE DE ADAUGAT ASSET-urile in DB(NUMELE ASSET-ului, ASSET ID-ul SI STAREA DE UTILIZARE)
+# FUNCTIE DE ADAUGAT ASSET-urile in DB(NUMELE ASSET-ului, ASSET ID-ul SI STAREA DE UTILIZARE)
 def update_assets_info(assets_list):
     cursor = None
     connection = None
     try:
-        connection = psycopg2.connect(host=hostname,dbname=database,user=username,password=pwd,port=port_id)
+        connection = psycopg2.connect(
+            host=hostname, dbname=database, user=username, password=pwd, port=port_id)
         cursor = connection.cursor()
         assets_to_add = []
         for asset in assets_list:
             if int(assets_list[f'{asset}'].get('assetID')) not in get_assets_in_db("all"):
-                assets_to_add.append([assets_list[f'{asset}'].get('assetID'),assets_list[f'{asset}'].get('assetName'),assets_list[f'{asset}'].get('isUsed')])
+                assets_to_add.append([assets_list[f'{asset}'].get('assetID'), assets_list[f'{asset}'].get(
+                    'assetName'), assets_list[f'{asset}'].get('isUsed')])
         insert_script = 'INSERT INTO assets_info (asset_id,asset_name,is_used) VALUES (%s,%s,%s)'
         for asset in assets_to_add:
-            insert_data = (asset[0],asset[1],asset[2])
-            cursor.execute(insert_script,insert_data)
+            insert_data = (asset[0], asset[1], asset[2])
+            cursor.execute(insert_script, insert_data)
         connection.commit()
     except Exception as error:
         print(error)
@@ -53,7 +55,7 @@ def update_assets_info(assets_list):
             connection.close()
         print("UPDATED ASSETS INFO")
 
-#FUNCTIE CARE RETURNEAZA O LISTA CU TOATE ID-urile ASSET-urilor DIN TABELA assets_info
+# FUNCTIE CARE RETURNEAZA O LISTA CU TOATE ID-urile ASSET-urilor DIN TABELA assets_info
 def get_assets_in_db(type):
     if type == "only_used_assets":
         script = 'SELECT asset_id FROM assets_info WHERE is_used=true'
@@ -62,10 +64,11 @@ def get_assets_in_db(type):
     cursor = None
     connection = None
     try:
-        connection = psycopg2.connect(host=hostname,dbname=database,user=username,password=pwd,port=port_id)
+        connection = psycopg2.connect(
+            host=hostname, dbname=database, user=username, password=pwd, port=port_id)
         cursor = connection.cursor()
         cursor.execute(script)
-        assets_in_db = [int(id[0]) for id in cursor.fetchall() ]
+        assets_in_db = [int(id[0]) for id in cursor.fetchall()]
     except Exception as error:
         print(error)
     finally:
@@ -75,39 +78,40 @@ def get_assets_in_db(type):
             connection.close()
         return assets_in_db
 
-#FUNCTIE DE EXTRAGERE IN FORMAT DE LISTA A VALORILOR DIN COGNITEDB IMPREUNA CU ORA, DATA SI ASSET ID-ul
-def get_datapoints_list_format(datapoints_id,asset_id):
-    datapoints = c.datapoints.retrieve(id=datapoints_id,start=datetime.now()-timedelta(days=refresh_time),end=datetime.now())
+# FUNCTIE DE EXTRAGERE IN FORMAT DE LISTA A VALORILOR DIN COGNITEDB IMPREUNA CU ORA, DATA SI ASSET ID-ul
+def get_datapoints_list_format(datapoints_id, asset_id):
+    datapoints = c.datapoints.retrieve(id=datapoints_id, start=datetime.now()-timedelta(days=refresh_time), end=datetime.now())
     data_list = []
-    i=0
+    i = 0
     for data in datapoints:
-        i+=1
+        i += 1
         data_to_dump = []
         data_to_dump.append(time.strftime('%d/%m/%Y %H:%M:%S', time.gmtime(data.timestamp/1000)))
         data_to_dump.append(data.value)
         data_to_dump.append(asset_id)
         data_list.append(data_to_dump)
-        if i==5:
+        if i == 5:
             break
     print("GOT NEW DATA LIST")
     return data_list
 
 
-
-#FUNCTIE DE ADAUGARE A DATELOR IN DB
+# FUNCTIE DE ADAUGARE A DATELOR IN DB
 def transfer_datapoints_to_db(assets_data):
     cursor = None
     connection = None
     counter = 0
     try:
-        connection = psycopg2.connect(host=hostname,dbname=database,user=username,password=pwd,port=port_id)
+        connection = psycopg2.connect(
+            host=hostname, dbname=database, user=username, password=pwd, port=port_id)
         cursor = connection.cursor()
         insert_script = 'INSERT INTO datapoints_info (timestamp,value,asset_id) VALUES (%s,%s,%s)'
         for data in assets_data:
             for individual_data in data:
-                insert_data = (individual_data[0],individual_data[1],individual_data[2])
+                insert_data = (
+                    individual_data[0], individual_data[1], individual_data[2])
                 counter += 1
-                cursor.execute(insert_script,insert_data)
+                cursor.execute(insert_script, insert_data)
         connection.commit()
     except Exception as error:
         print(error)
@@ -118,7 +122,7 @@ def transfer_datapoints_to_db(assets_data):
             connection.close()
         print(f"ADDED NEW DATA TO DB\nNumber of entries: {counter}")
 
-#FUNCTIE PRINCIPALA - APELEAZA FUNCTIILE NECESARE PENTRU UPDATE-ul DB-ului
+# FUNCTIE PRINCIPALA - APELEAZA FUNCTIILE NECESARE PENTRU UPDATE-ul DB-ului
 def add_new_datapoints_to_db():
     print("\nStarted script")
     astInf = open('DataIngestion/assets.json')
@@ -128,14 +132,15 @@ def add_new_datapoints_to_db():
     assets_data = []
     for asset in assets_list:
         if int(assets_list[f'{asset}'].get('assetID')) in active_assets_id:
-            assets_data.append(get_datapoints_list_format(int(assets_list[f'{asset}'].get('datapointsID')),int(assets_list[f'{asset}'].get('assetID'))))
+            assets_data.append(get_datapoints_list_format(int(assets_list[f'{asset}'].get(
+                'datapointsID')), int(assets_list[f'{asset}'].get('assetID'))))
     transfer_datapoints_to_db(assets_data)
     print("FINISHED SCRIPT")
 
-#APELARE INITIALA
-
+# APELARE INITIALA
 add_new_datapoints_to_db()
-#SCHEDULER CARE APELEAZA FUNCTIA PRINCIPALA LA FIECARE 30 DE SECUNDE
+
+# SCHEDULER CARE APELEAZA FUNCTIA PRINCIPALA LA FIECARE 30 DE SECUNDE
 schedule.every(refresh_time).days.do(add_new_datapoints_to_db)
 while True:
     schedule.run_pending()
--- RestAPI_BackEnd/RESTful.py
+++ RestAPI_BackEnd/RESTful.py
@@ -1,46 +1,78 @@
+from datetime import datetime, timedelta
+from queue import Empty
 from statistics import mean
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import values
 import json
 
-#DATABASE INFO
+# DATABASE INFO
 dbInf = open('DataIngestion/databaseInfo.json')
 db_info = json.load(dbInf)
 hostname = db_info['hostname']
 database = db_info['databaseName']
 username = db_info['username']
-pwd      = db_info['password']
-port_id  = db_info['portID']
+pwd = db_info['password']
+port_id = db_info['portID']
 dbInf.close()
 
-#CREAREA APLICATIEI FLASK SI SETUPul DBului
-app = Flask(__name__)
+# CREAREA APLICATIEI FLASK SI SETUPul DBului
+app = Flask(__name__, template_folder='../FrontEnd/templates',static_folder='../static')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{username}:{pwd}@{hostname}:{port_id}/{database}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db  = SQLAlchemy(app)
-asset = db.Table('assets_info', db.metadata, autoload = True, autoload_with = db.engine)
-data = db.Table('datapoints_info',db.metadata,autoload = True, autoload_with = db.engine)
+db = SQLAlchemy(app)
+asset = db.Table('assets_info', db.metadata, autoload=True, autoload_with=db.engine)
+data = db.Table('datapoints_info', db.metadata, autoload=True, autoload_with=db.engine)
 
-@app.route('/')
+
+@app.route('/home')
 def index():
-    return '' 
+    return render_template('home.html')
 
-#FUNCTIE DE AFISARE A TUTUTOR ASSETurilor DIN DB
+# FUNCTIE DE AFISARE A TUTUTOR ASSETurilor DIN DB
 @app.route('/assets')
 def get_assets():
     results = db.session.query(asset).all()
     output = []
-    for ass in results:
-        asset_name = {'name':ass.asset_name}
+    for asst in results:
+        asset_name = {'name': asst.asset_name}
         output.append(asset_name)
-    return {'assets':output}
+    return render_template('assets.html', posts=output)
 
-#FUNCTIE DE CALCUL A MEDIEI VALORILOR UNUI ASSET IN FUNCTIE DE ID
-@app.route('/average/<asset_id>')
-def get_values_average(asset_id):
+# FUNCTIE DE CALCUL A MEDIEI VALORILOR UNUI ASSET IN FUNCTIE DE ID
+@app.route('/average/<asset_id>/<int:interval>')
+def get_values_average(asset_id,interval):
+    start=datetime.now()-timedelta(days=int(interval))
     results = db.session.query(data).filter_by(asset_id = asset_id).all()
     values = []
-    for ass in results:
-        values.append(ass.value)
-    return {'average':mean(values)}
+    for asst in results:
+        if (datetime.strptime(str(asst.timestamp), '%Y-%d-%m %H:%M:%S') > start):
+            values.append(asst.value)
+    if not values:
+        return f'No data from {start}'
+    return f"Average: {mean(values)} starting from {start}"
+
+# FUNCTIE DE CALCUL A MAXIMULUI VALORILOR UNUI ASSET IN FUCNTIE DE ID
+@app.route('/maximum/<asset_id>/<int:interval>')
+def get_values_maximum(asset_id,interval):
+    start=datetime.now()-timedelta(days=int(interval))
+    results = db.session.query(data).filter_by(asset_id = asset_id).all()
+    values = []
+    for asst in results:
+        if (datetime.strptime(str(asst.timestamp), '%Y-%d-%m %H:%M:%S') > start):
+            values.append(asst.value)
+    if not values:
+        return f'No data from {start}'
+    return f"Maximum: {max(values)} starting from {start}"
+
+#FUNCTIE CARE RETURNEAZA DATAPOINTurile UNUI ASSET IN FUNCTIE DE ID
+@app.route('/datapoints/<asset_id>')
+def get_values(asset_id):
+    results = db.session.query(data).filter_by(asset_id=asset_id).all()
+    values = []
+    for asst in results:
+        values.append(f"{asst.value},{asst.timestamp}")
+    return {'datapoints': values}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
